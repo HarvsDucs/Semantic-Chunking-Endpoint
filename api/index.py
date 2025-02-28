0727ddaaf88851fdffcbe3e7d2a4b5b0bda12096
HarvsDucs <124319998+HarvsDucs@users.noreply.github.com>
2025-02-28 16:07:42 +0800

    adding supabase stuff

--- api/index.py
+++ api/index.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify 
 from langchain_experimental.text_splitter import SemanticChunker
 from langchain_openai import OpenAIEmbeddings
+import os 
+from supabase import create_client, Client
 
 app = Flask(__name__)
 
